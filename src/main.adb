f2c2110888910f11053209e364fd6c357eb27b1f
Hal Smith <hal@schnibb.com>
2021-02-02 12:59:20 +0000

    Finished section 3: array types. Added gitignore for generated files.

--- src/main.adb
+++ src/main.adb
@@ -6,33 +6,32 @@ procedure Main is
 
    Coord_Offset : constant Integer := 100;
 
-   type X_Coord is range 0 .. 360;
-   type Y_Coord is range 0 .. 360;
+   type X_Coord is range 0 .. 7;
+   type Y_Coord is range 0 .. 15;
 
    Red_Box : Shape_Id;
 
-   Box_Size : constant Natural := 10;
+   Box_Size : constant Float := Float (Coord_Offset * 2) / Float (Y_Coord'Last);
 
+   X_Pos : X_Coord := 4;
    Y_Pos : Y_Coord := 0;
 
    type Cell_Type is (Empty, I, O, J, L, S, T, Z);
 
-   type Row_Dims is range 0 .. 7;
+   type Row_Array is array (X_Coord) of Cell_Type;
 
-   type Row_Array is array (Row_Dims) of Cell_Type;
-
-   type Board_Array is array (Row_Dims) of Row_Array;
+   type Board_Array is array (Y_Coord) of Row_Array;
 
    The_Board : Board_Array;
 
    function Translate_X (X_Left : in X_Coord) return Float is
    begin
-      return Float (X_Left) - Float (Coord_Offset);
+      return Box_Size * Float (X_Left) - Float (Coord_Offset);
    end Translate_X;
 
    function Translate_Y (Y_Bottom : in Y_Coord) return Float is
    begin
-      return Float (Coord_Offset) - Float (Y_Bottom);
+      return Float (Coord_Offset) - Box_Size * Float (Y_Bottom);
    end Translate_Y;
 
 begin
@@ -41,23 +40,25 @@ begin
       Row := (others => Empty);
    end loop;
 
-   Red_Box := New_Box (X      => Translate_X (0),
+   Red_Box := New_Box (X      => Translate_X (X_Pos),
                        Y      => Translate_Y (Y_Pos),
-                       Width  => Float (Box_Size),
-                       Height => Float (Box_Size),
+                       Width  => Box_Size,
+                       Height => Box_Size,
                        Color  => Red);
 
-   while Y_Pos <= 200 loop
+   loop
       Set_Y (Shape => Red_Box,
              Value => Translate_Y (Y_Pos));
       delay 0.1;
-      Y_Pos := Y_Pos + Y_Coord (Box_Size);
+      exit when Y_Pos = Y_Coord'Last;
+      Y_Pos := Y_Pos + 1;
+      Put_Line (Y_Pos'Image);
    end loop;
 
    New_Line;
    Put_Line ("   GAME OVER");
 
-   The_Board (7)(0) := Z;
+   The_Board (Y_Pos)(X_Pos) := Z;
 
    for Row of The_Board loop
       for Cell of Row loop
