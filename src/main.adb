6092a57fc2e661a4ad4c6d20e1bc35e9162059a0
Hal Smith <hal@schnibb.com>
2021-02-02 15:47:03 +0000

    Finished Lab 4.

--- src/main.adb
+++ src/main.adb
@@ -9,8 +9,6 @@ procedure Main is
    type X_Coord is range 0 .. 7;
    type Y_Coord is range 0 .. 15;
 
-   Red_Box : Shape_Id;
-
    Box_Size : constant Float := Float (Coord_Offset * 2) / Float (Y_Coord'Last);
 
    X_Pos : X_Coord := 4;
@@ -18,11 +16,29 @@ procedure Main is
 
    type Cell_Type is (Empty, I, O, J, L, S, T, Z);
 
-   type Row_Array is array (X_Coord) of Cell_Type;
+   subtype Piece_Type is Cell_Type range I .. Z;
+
+   --  Colour_Array is array () ;
+   type Falling_Piece is record
+      Content_Type : Piece_Type;
+      Shape : Shape_Id;
+      X_Pos : X_Coord := 3;
+      Y_Pos : Y_Coord := 0;
+   end record;
+
+   type Cell_Content is record
+      Content_Type : Cell_Type := Empty;
+      Piece : Falling_Piece;
+   end record;
+
+   Piece_1 : Falling_Piece;
+
+   type Row_Array is array (X_Coord) of Cell_Content;
 
    type Board_Array is array (Y_Coord) of Row_Array;
 
    The_Board : Board_Array;
+   Board_Copy : Board_Array;
 
    function Translate_X (X_Left : in X_Coord) return Float is
    begin
@@ -36,36 +52,41 @@ procedure Main is
 
 begin
 
-   for Row of The_Board loop
-      Row := (others => Empty);
-   end loop;
+   Piece_1.Content_Type := Z;
+   Piece_1.Shape := New_Box (X      => Translate_X (Piece_1.X_Pos),
+                             Y      => Translate_Y (Piece_1.Y_Pos),
+                             Width  => Box_Size,
+                             Height => Box_Size,
+                             Color  => Red);
+   The_Board (Piece_1.Y_Pos)(Piece_1.X_Pos) := (Content_Type => Piece_1.Content_Type,
+                                                Piece => Piece_1);
 
-   Red_Box := New_Box (X      => Translate_X (X_Pos),
-                       Y      => Translate_Y (Y_Pos),
-                       Width  => Box_Size,
-                       Height => Box_Size,
-                       Color  => Red);
-
-   loop
-      Set_Y (Shape => Red_Box,
-             Value => Translate_Y (Y_Pos));
+   while Piece_1.Y_Pos < Y_Coord'Last loop
       delay 0.1;
-      exit when Y_Pos = Y_Coord'Last;
-      Y_Pos := Y_Pos + 1;
-      Put_Line (Y_Pos'Image);
+      Board_Copy := The_Board;
+
+      for Row of Board_Copy loop
+         for Cell of Row loop
+            if Cell.Content_Type /= Empty and then Cell.Piece.Y_Pos < Y_Coord'Last then
+               Cell.Piece.Y_Pos := Cell.Piece.Y_Pos + 1;
+               Set_Y (Shape => Cell.Piece.Shape,
+                      Value => Translate_Y (Cell.Piece.Y_Pos));
+               The_Board (Cell.Piece.Y_Pos)(Cell.Piece.X_Pos) := (Piece => Cell.Piece,
+                                                                  Content_Type => Cell.Piece.Content_Type);
+            end if;
+         end loop;
+      end loop;
    end loop;
 
    New_Line;
    Put_Line ("   GAME OVER");
 
-   The_Board (Y_Pos)(X_Pos) := Z;
-
    for Row of The_Board loop
       for Cell of Row loop
-         if Cell = Empty then
+         if Cell.Content_Type = Empty then
             Put ("-");
          else
-            Put (Cell'Image);
+            Put (Cell.Content_Type'Image);
          end if;
          Put (" ");
       end loop;
